e264c6052eec61f5c0857a8b55140998b15fdd8b
WIN-IK381US42M3\Администратор <ilyaantar@yandex.ru>
2021-07-05 22:18:45 +0300

    Чистка

--- script/birth/birth_conf.lua
+++
@@ -1,4 +0,0 @@
-ClearAllBirthPoint()
---格式        地名      地图名       地图坐标(单位:米)
-AddBirthPoint("Argent City", "garner",  2186, 2781)
-
--- script/help/addhelpnpc.lua
+++
@@ -1,5 +0,0 @@
---初始化帮助NPC
---此文件在GameServer地图初始化全部完成之后, 载入一次
-
-ClearHelpNPC()
-AddHelpNPC("Drunkyard - Anthony")
